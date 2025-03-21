5457fd38372f0d4323b5bf53c524cd8f0ee238b7
Lydiouche <lydia.lefebvremonn@gmail.com>
2025-03-21 13:13:07 +0100

    Add files via upload

---
+++ date.adb
@@ -0,0 +1,108 @@
+WITH Ada.Text_IO, Ada.Integer_Text_IO;
+USE Ada.Text_Io, Ada.Integer_Text_IO;
+
+PACKAGE BODY Date IS
+
+   FUNCTION Bis (
+         Annee : Integer)
+     RETURN Boolean IS
+      Bissextile : Boolean;
+   BEGIN
+      IF (Annee mod 4 = 0 AND (Annee mod 100 /= 0 OR Annee mod 400 = 0))THEN
+         Bissextile:= True;
+      ELSE
+         Bissextile:= False;
+      END IF;
+      RETURN Bissextile;
+   END Bis;
+
+   FUNCTION Nbj (
+         M     : T_Mois;
+         Annee : Integer)
+     RETURN Integer IS
+
+   BEGIN
+      CASE M IS
+         WHEN Janvier | Mars | Mai | Juillet | Aout | Octobre | Decembre =>
+            RETURN 31;
+         WHEN Avril | Juin | Septembre | Novembre =>
+            RETURN 30;
+         WHEN Fevrier =>
+            IF Bis(annee) THEN
+               RETURN 29;
+            ELSE
+               RETURN 28;
+            END IF;
+      END CASE;
+   END Nbj;
+
+   PROCEDURE Saisie_date (
+         T :    OUT T_Date) IS
+      S : String (1 .. 10);
+      K : Integer;
+
+   BEGIN
+      LOOP
+         begin
+         Put_Line("Veuillez saisir l'annee:");
+         Get(T.Annee);
+            Skip_Line;
+         EXIT WHEN T.Annee > 0;
+         EXCEPTION
+         WHEN Data_Error => Skip_Line;Put_Line("erreur de saisie, recommencez");
+         end;
+      END LOOP;
+      LOOP
+         begin
+         Put_Line("Veuillez saisir le mois (en lettres):");
+         Get_Line(S,K);
+         T.Mois:= T_Mois'Value(S(1..K));
+         EXIT;
+         EXCEPTION
+         WHEN Constraint_Error=>Put_Line("erreur de saisie");
+         end;
+      end loop;
+      LOOP
+         begin
+         Put_Line("Veuillez saisir le jour (nombre):");
+         Get(T.Numj);
+         Skip_Line;
+         EXIT WHEN T.Numj <= Nbj(T.Mois,T.Annee);
+         EXCEPTION
+         When constraint_error => skip_line;put_line("Mauvaise valeur, recommencez");
+         WHEN Data_Error => Skip_Line;Put_Line("Erreur de saisie, recommencez");
+         end;
+      END LOOP;
+   END Saisie_date;
+
+   PROCEDURE Lendemain (
+         T : IN OUT T_Date) IS
+   BEGIN
+      IF T.Numj < Nbj(T.Mois,T.Annee) THEN
+         T.Numj:= T.Numj +1;
+      ELSE
+         T.Numj:=1;
+         IF T.Mois = T_Mois'Last THEN
+            T.Mois:= T_Mois'First;
+            T.annee:=T.annee + 1;
+         ELSE
+            T.Mois:= T_Mois'succ(T.mois);
+         END IF;
+      END IF;
+   END Lendemain;
+
+
+      PROCEDURE affiche_date(
+            T : IN     T_Date) IS
+                     BEGIN
+         Put(T.Numj,1);
+         Put(" -- ");
+         Put(T_Mois'Image(T.Mois));
+         Put(" -- ");
+         Put(T.Annee,1);
+
+      END affiche_date;
+   END Date;
+
+
+
---
+++ date.ads
@@ -0,0 +1,23 @@
+PACKAGE Date IS
+
+-- Type pour la Date
+   TYPE T_mois is (Janvier, Fevrier, Mars, Avril, Mai, Juin, Juillet, Aout, Septembre, Octobre, Novembre, Decembre) ;
+   SUBTYPE Intj IS Integer RANGE 1..31 ; -- pour le nombre de jours dans un mois
+   TYPE T_Date IS RECORD -- pour la date entière
+      Numj : Intj ;
+      Mois : T_Mois ;
+      Annee : Positive ;
+   END RECORD ;
+
+
+   PROCEDURE Saisie_Date (T: OUT T_Date);
+   -- Au début du logiciel utilisateur saisie la date du jour
+   FUNCTION Bis(Annee: integer) RETURN Boolean;
+   FUNCTION Nbj( M:T_Mois; Annee: Integer) RETURN Integer;
+   -- Fonctions de vérifications de la saisie du jour
+   -- Procédure de passage au lendemain
+   PROCEDURE Affiche_Date(T: IN T_Date); -- Besoin pour afficher la date du jour et autres dates
+   PROCEDURE Lendemain (T : IN OUT T_Date);
+   -- Actualise le jour au lendemain // passage au lendemain
+END Date;
+
