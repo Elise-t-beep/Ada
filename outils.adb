d62fbba9442d0a0aba1dd028a953c0c911c4cff8
Lydiouche <lydia.lefebvremonn@gmail.com>
2025-03-24 14:33:08 +0100

    Add files via upload

--- outils.adb
+++ outils.adb
@@ -1,21 +1,55 @@
 PACKAGE BODY Outils IS
 
------------------------------------------------------------------------------------------------------
-
-   PROCEDURE Saisie_Mot (M : OUT T_Mot; K : OUT Integer) IS
+   -----------------------------------------------------------------------------------------------------
+   PROCEDURE Saisie_Mot (
+         M :    OUT T_Mot;
+         K :    OUT Integer) IS
       --procedure afin de verifier que tous les mots saisis sont "grammaticalement" corrects
+      Ok : Boolean := False;
+   BEGIN
+      LOOP
+         M := (OTHERS =>' ');
+         Get_Line (M, K);
+         M := To_Upper (M);
+
+         --validation de l'ensemble des characteres du mot ecrit
+         FOR I IN 1..K LOOP
+            CASE M(I) IS
+               WHEN 'A'..'Z' =>
+                  Ok := True;
+               WHEN '-'|'''|' ' =>
+                  IF I= 1 OR I=K THEN
+                     Put_Line ("Erreur de saisie, pas de caracteres speciaux aux extremites ");
+                     Ok := False;
+                     EXIT;
+                  ELSIF M(I-1)='-' OR M(I-1)=''' OR M(I-1)=' ' THEN
+                     Put_Line ("Erreur de saisie, pas de caracteres speciaux a la suite ");
+                     Ok := False;
+                     EXIT;
+                  ELSE Ok:=True;
+                  END IF;
+               WHEN OTHERS =>
+                  Put ("Erreur de saisie, erreur, caracteres non autorises ");
+                  Ok := False;
+                  EXIT;
+            END CASE;
+         END LOOP;
+         EXIT WHEN OK=True;
+      END LOOP;
+
+   END Saisie_Mot;
+   -----------------------------------------------------------------------------------------------------
+   PROCEDURE Saisie_Titre (
+         M :    OUT T_Titre;
+         K :    OUT Integer) IS
       Ok : Boolean := False;
    BEGIN
       LOOP
          --invit de saisie dans le programme principal
          M := (OTHERS =>' ');
-         Put ("saisie : ");
+         Put ("Saisie : ");
          Get_Line (M, K);
-         M := To_Upper (M); --ou To_upper (mot(1..k));
-         --         Ok := True; --ce boolean permet de valider la saisie controlee
-
-         --         IF K = M'Length THEN Skip_Line;
-         --         END IF;
+         M := To_Upper (M);
 
          --validation de l'ensemble des characteres du mot ecrit
          FOR I IN M'First..K LOOP
@@ -41,54 +75,11 @@ PACKAGE BODY Outils IS
          EXIT WHEN OK=True;
       END LOOP;
 
-   END Saisie_Mot;
-
------------------------------------------------------------------------------------------------------
-
-   PROCEDURE Saisie_Titre (T : OUT T_Titre; K : OUT Integer) IS
-      --procedure afin de verifier que tous les mots saisis sont "grammaticalement" corrects
-      Ok : Boolean := False;
-   BEGIN
-      LOOP
-         --invit de saisie dans le programme principal
-         T := (OTHERS =>' ');
-         Put ("saisie : ");
-         Get_Line (T, K);
-         T := To_Upper (T); --ou To_upper (mot(1..k));
-         --         Ok := True; --ce boolean permet de valider la saisie controlee
-
-         --         IF K = M'Length THEN Skip_Line;
-         --         END IF;
-
-         --validation de l'ensemble des characteres du mot ecrit
-         FOR I IN T'First..K LOOP
-            CASE T(I) IS
-               WHEN 'A'..'Z' =>
-                  Ok := True;
-               WHEN '-'|'''|' ' =>
-                  IF I=T'First OR I=K THEN
-                     Put_Line ("pas de caracteres speciaux aux extremites ");
-                     Ok := False;
-                     EXIT;
-                  ELSIF T(I-1)='-' OR T(I-1)=''' OR T(I-1)=' ' THEN
-                     Put_Line ("pas de caracteres speciaux a la suite ");
-                     Ok := False;
-                     EXIT;
-                  END IF;
-               WHEN OTHERS =>
-                  Put ("erreur, caracteres non autorises ");
-                  Ok := False;
-                  EXIT;
-            END CASE;
-         END LOOP;
-         EXIT WHEN OK=True;
-      END LOOP;
-
    END Saisie_Titre;
 
------------------------------------------------------------------------------------------------------
-
-   PROCEDURE Saisie_Identite (Id : OUT T_Pers) IS
+   -----------------------------------------------------------------------------------------------------
+   PROCEDURE Saisie_Identite (
+         Id :    OUT T_Pers) IS
       --se servir de saisie_mot et de t_pers
    BEGIN
       Put_Line ("saisie de l'identite du membre (patient ou personnel : ");
@@ -99,32 +90,17 @@ PACKAGE BODY Outils IS
       Saisie_Mot(Id.Prenom, Id.Kprenom);
       Id.Prenom := To_Upper(Id.Prenom);
    END Saisie_Identite;
+   -----------------------------------------------------------------------------------------------------
+   PROCEDURE Creation_Login (Id : IN out T_Pers; Login : OUT T_Titre; Klogin : OUT Integer)IS
 
------------------------------------------------------------------------------------------------------
-
-   FUNCTION Calcul_Empreinte (Mdp : T_Mdp) RETURN Integer IS
-      Emp : Integer := 0;
    BEGIN
-      FOR I IN Mdp'RANGE LOOP
-         Emp := (Emp + (I * Character'Pos (Mdp(I)))) mod 2048;
-      END LOOP;
-      RETURN (Emp);
-   END Calcul_Empreinte;
+      ID.Nom:= To_Upper(Id.Nom);
+      ID.Prenom:= To_Upper(Id.Prenom);
+      Login(1..Id.Knom):= Id.Nom(1..ID.Knom);
+      Login(Id.Knom+1):='_';
+      Login(id.knom+2..id.knom+id.kprenom+1):= Id.preNom(1..ID.Kprenom);
+      Klogin:=Id.Kprenom+Id.Knom+1;
+   END CREATION_Login;
 
------------------------------------------------------------------------------------------------------
-
-   PROCEDURE Init_Empreinte (Emp : OUT Integer) IS
-      Mdp : T_Mdp;
-   BEGIN
-      --faire les invit ?
-      --verif du login = mdp ?
-      --saisie que si admin (faire verif ?)
-      -- faire une visu de l'empreinte faite?
-      Put ("saisie du mdp (10 caracteres) : ");
-      Get (Mdp); Skip_Line;
-      Emp := Calcul_Empreinte (Mdp);
-   END Init_Empreinte;
-
-
-----------------------------------------------------------------------------------------------
 END Outils;
+
--- outils.ads
+++ outils.ads
@@ -1,29 +1,32 @@
 WITH Ada.Characters.Handling, Ada.Text_IO;
 USE Ada.Characters.Handling, Ada.Text_Io;
 
-
-Package Outils is
+PACKAGE Outils IS
 
    SUBTYPE T_Mot IS String(1..20);
 
-   TYPE T_Pers IS RECORD
-      Nom : T_Mot := (OTHERS =>' ');
-      Knom : Integer;
-      Prenom : T_Mot := (OTHERS =>' ');
-      Kprenom : Integer;
-   END RECORD;
+   TYPE T_Pers IS
+      RECORD
+         Nom     : T_Mot   := (OTHERS => ' ');
+         Knom    : Integer;
+         Prenom  : T_Mot   := (OTHERS => ' ');
+         Kprenom : Integer;
+      END RECORD;
 
    SUBTYPE T_Titre IS String(1..50); --titre des docs
    SUBTYPE T_Mdp IS String(1..10); --chaine de caractere de 10 pour mdp
 
-   PROCEDURE Saisie_Mot (M : OUT T_Mot; K : OUT Integer);
-   PROCEDURE Saisie_Identite (Id: OUT T_Pers);
-   PROCEDURE Init_Empreinte (Emp : OUT Integer);
-   FUNCTION Calcul_Empreinte (Mdp : T_Mdp) RETURN Integer;
-   PROCEDURE Saisie_Titre (T : OUT T_Titre; K : OUT Integer);
+   PROCEDURE Saisie_Mot (
+         M :    OUT T_Mot;
+         K :    OUT Integer);
+   PROCEDURE Saisie_Identite (
+      Id :    OUT T_Pers);
 
-
+   PROCEDURE Saisie_Titre (
+         M :    OUT T_Titre;
+      K :    OUT Integer);
+   PROCEDURE Creation_Login (Id : IN out T_Pers; Login : OUT T_Titre; Klogin : OUT Integer);
+   --faire saisie login-mdp + verif ici ou dans d'autres packages???
 
 END Outils;
-
 
